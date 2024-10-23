05a5b762cccba543f9ef2ba19ed374961decdf8e
Bruno-Otavio <brunotavio.contatoescola@gmail.com>
2024-10-23 16:07:34 -0300

    notificatioons working

--- app/professor/lib/services/firebase_messaging_service.dart
+++ app/professor/lib/services/firebase_messaging_service.dart
@@ -8,6 +8,28 @@ class FirebaseMessagingService {
 
     final fCMToken = await _firebaseMessaging.getToken();
 
-    print('Token: $fCMToken');
+    initNotifications();
+  }
+
+  Future initPushNotifications() async {
+    _firebaseMessaging.getInitialMessage().then(handleMessage);
+
+    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Notificação: ${message.notification?.title}');
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
+  }
+
+  Future<void> handleBackgroundMessage(RemoteMessage message) async {
+    print('Title: ${message.notification?.title}');
+  }
+
+  void handleMessage(RemoteMessage? message) {
+    if (message == null) return;
+
+    print('Notificação clicada');
   }
 }
